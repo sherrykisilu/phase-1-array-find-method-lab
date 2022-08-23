9ae74e0f6981326a67c579880428214054db02c9
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-23 22:57:26 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+    //...
+  ]
+   function superbowlWin(records){
+    let win = records.find((record) =>record.result==="W");
+    if (typeof win==='undefined'){
+        return undefined;
+    }
+    else{
+        return win.year;
+    }
+   }
+  console.log (superbowlWin(record));
